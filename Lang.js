9785eb85ce35ed2b40c10227382f12dc3cb9d6a6
Seanitzel <seandvir12@gmail.com>
2018-07-10 03:32:04 +0300

    updated all scale constants and classes to match them

--- Lang.js
+++ Lang.js
@@ -1,22 +1,41 @@
-const major_scale = [2, 2, 1, 2, 2, 2, 1];
-const minor_scale = [2, 1, 2, 2, 1, 2, 2];
-const Minor_pentatonic_scale = [3, 2, 2, 3, 2];
-const Blues_scale = [3, 2, 1, 1, 3, 2];
-const Major_pentatonic_scale = [2, 2, 3, 2, 3];
-const Harmonic_Minor_scale = [2, 1, 2, 2, 1, 3, 1];
-const Melodic_Minor_scale = [2, 1, 2, 2, 2, 2, 1];
-const Ionian_scale = [2, 2, 1, 2, 2, 2, 1];
-const Dorian_scale = [2, 1, 2, 2, 2, 1, 2];
-const Phrygian_scale = [1, 2, 2, 2, 1, 2, 2];
-const Lydian_scale = [2, 2, 2, 1, 2, 2, 1];
-const Mixolydian_scale = [2, 2, 1, 2, 2, 1, 2];
-const Aeolian_scale = [2, 1, 2, 2, 1, 2, 2];
-const Locrian_scale = [1, 2, 2, 1, 2, 2, 2];
-const Whole_tone_scale = [2, 2, 2, 2, 2, 2];
-const Whole_Half_Diminished = [2, 1, 2, 1, 2, 1, 2, 1];
-const Half_Whole_Diminished = [1, 2, 1, 2, 1, 2, 1, 2];
+// const major_scale = [2, 2, 1, 2, 2, 2, 1];
+// const minor_scale = [2, 1, 2, 2, 1, 2, 2];
+// const Minor_pentatonic_scale = [3, 2, 2, 3, 2];
+// const Blues_scale = [3, 2, 1, 1, 3, 2];
+// const Major_pentatonic_scale = [2, 2, 3, 2, 3];
+// const Harmonic_Minor_scale = [2, 1, 2, 2, 1, 3, 1];
+// const Melodic_Minor_scale = [2, 1, 2, 2, 2, 2, 1];
+// const Ionian_scale = [2, 2, 1, 2, 2, 2, 1];
+// const Dorian_scale = [2, 1, 2, 2, 2, 1, 2];
+// const Phrygian_scale = [1, 2, 2, 2, 1, 2, 2];
+// const Lydian_scale = [2, 2, 2, 1, 2, 2, 1];
+// const Mixolydian_scale = [2, 2, 1, 2, 2, 1, 2];
+// const Aeolian_scale = [2, 1, 2, 2, 1, 2, 2];
+// const Locrian_scale = [1, 2, 2, 1, 2, 2, 2];
+// const Whole_tone_scale = [2, 2, 2, 2, 2, 2];
+// const Whole_Half_Diminished = [2, 1, 2, 1, 2, 1, 2, 1];
+// const Half_Whole_Diminished = [1, 2, 1, 2, 1, 2, 1, 2];
 
-const scale_intervals = ['unison', 'second', 'third'];
+const major_scale = [2, 4, 5, 7, 9, 11, 12];
+const minor_scale = [2, 3, 5, 7, 8, 10, 12];
+const Harmonic_Minor_scale = [2, 3, 5, 7, 8, 11, 12];
+const Melodic_Minor_scale = [2, 3, 5, 7, 9, 11, 12];
+const Minor_pentatonic_scale = [3, 5, 7, 10, 12];
+const Blues_scale = [3, 5, 6, 7, 10, 12];
+const Major_pentatonic_scale = [2, 4, 7, 9, 12];
+const Ionian_scale = major_scale;
+const Dorian_scale = [2, 3, 5, 7, 9, 10, 12];
+const Phrygian_scale = [1, 3, 5, 7, 8, 10, 12];
+const Lydian_scale = [2, 4, 6, 7, 9, 11, 12];
+const Mixolydian_scale = [2, 4, 5, 7, 9, 10, 12];
+const Aeolian_scale = [2, 3, 5, 7, 8, 10, 12];
+const Locrian_scale = [1, 3, 5, 6, 8, 10, 12];
+const Whole_tone_scale = [2, 4, 6, 8, 10, 12];
+const Whole_Half_Diminished = [2, 3, 5, 6, 8, 9, 11, 12];
+const Half_Whole_Diminished = [1, 3, 4, 6, 7, 9, 10, 12];
+
+const scale_intervals = ['unison', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'nineth', 'tenth', 'eleventh'];
+const scales = ['Major', 'Minor', 'harmonic Minor', 'Melodic Minor', 'Ionian', 'Dorian', 'Phrygian', 'Lydian', 'Mixolydian', 'Aeolian', 'Locrian', 'Major Pentatonic', 'Minor Pentatonic', 'Whole Tone', 'Whole Half Diminished', 'Half Whole Diminished'];
 
 const major_chord = [4, 7];
 
@@ -27,19 +46,34 @@ const notes = {
     'b': ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
 };
 
+const add = (a, b) => a + b;
+
+function toString(data) {
+    let formatted_data = "";
+    for (let i = 0; i < data.length - 2; ++i)
+        formatted_data += data[i] + ', ';
+    formatted_data += data[data.length - 1];
+    return formatted_data;
+}
+
 class Scale {
     constructor(tonic, formula) {
         this.tonic = new Note(tonic);
         this.formula = formula;
-        let interval = 0;
         this.notes = [tonic];
         for (let i of formula) {
-            interval += i;
-            this.notes.push(this.tonic.getInterval(interval));
+            this.notes.push(this.tonic.getInterval(i));
         }
     }
     getNotes() {
         return toString(this.notes);
+    }
+    getInterval(interval) {
+        if (typeof(interval) == "string") {
+
+        } else {
+            return this.tonic.getInterval(this.formula[interval - 2]);
+        }
     }
     unison() {
         return this.tonic.getInterval(0);
@@ -65,13 +99,6 @@ class Scale {
     eighth() {
         return this.tonic.getInterval(this.formula[7]);
     }
-}
-
-function toString(data) {
-    let formatted_data = "";
-    for (let i of data)
-        formatted_data += i + ', ';
-    return formatted_data;
 }
 
 class Note {
@@ -101,16 +128,16 @@ class Note {
         return chord;
     }
     getMajorScale() {
-        let interval = 0;
         let scale = [this.note];
         for (let i of major_scale) {
-            interval += i;
-            scale.push(this.getInterval(interval));
+            scale.push(this.getInterval(i));
         }
         return toString(scale);
     }
 }
 
-// let a = new Note('A', '5');
+let n = new Note('A', '5');
+let c = new Scale('C', major_scale);
 // console.log(a.frequency(), a.getMajorChord(), (new Note('C', '3')).getMajorScale());
 // console.log((new Note('C', '3')).getMajorScale());
+console.log(c.getInterval(5));
